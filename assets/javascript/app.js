426582e0c00277fd5a54780d087b3c2f0d538c7a
Jacksona750 <jacksona750@gmail.com>
2019-02-04 20:04:44 -0800

    forms added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,8 @@ var address = "2029 olympic blvd santa monica, ca 90404"
 
 // This is our API key. Add your own API key between the ""
 var weatherAPI = "265a46d65db9c9a8b164aa9180136f67";
-var mapsAPI ="OEeXUFJsMGPGX6jvueAh5pJ2YyUCzbay"
+var mapsAPI = "OEeXUFJsMGPGX6jvueAh5pJ2YyUCzbay";
+var restAPI = "DOF99u8HLWYBY7XS57PcCuQ6aJdh0eWYQVSSRXCj3oWg-OPD8t6QIKTHMkbL-uuJhmVNE-by-DG2tz7bQndWFBjgc_-AF1wbFUD2tLDFQNVrUmAsEHRnJVzA_gZZXHYx";
 var cityName = "burbank";
 
 // Here we are building the URL we need to query the weather database
@@ -65,6 +66,23 @@ $.ajax({
     // Create CODE HERE to log the resulting object
     console.log(response)
   })
+
+// Buidling the URL to query the TomTom database for route duration
+var restaurantQuery = "https://api.yelp.com/v3/businesses/search" + originLat + "," + originLong + ":" + destLat + "," + destLong +"/json?&key=" + mapsAPI;
+
+// AJAX call for location
+$.ajax({
+    url: restaurantQuery,
+    method: "GET"
+  }).then(function(response) {
+  
+    // Create CODE HERE to Log the locationQuery
+    console.log(durationQuery)
+    // Create CODE HERE to log the resulting object
+    console.log(response)
+  })
+
+
 $(document).ready(function(){
     $(".sign-in-select").on("click", function(){
         console.log($(this));
@@ -89,4 +107,17 @@ $(document).ready(function(){
 
         $(".submit-btn").text("Sign Up");
     });
+    
+    // Hide new address on page load
+    // $("#new-address").hide();
+
+    // On click function to show address input for add event
+    $("#add-location").change(function(){
+        $("#new-address").show();
+    });
+
+    // On click function to hide address input for add event
+    $("#default-location").change(function(){
+      $("#new-address").hide();
+  });
 });
